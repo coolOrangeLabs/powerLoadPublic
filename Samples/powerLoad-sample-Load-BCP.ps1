bcf674bf33cd7fb49da323dc24e7eb8a30a1dfa6
Markus Dössinger <36193155+MarkusCool@users.noreply.github.com>
2025-01-20 11:22:15 +0100

    Add files via upload
    Update sample file for loading BCP-package

--- Samples/powerLoad-sample-Load-BCP.ps1
+++ Samples/powerLoad-sample-Load-BCP.ps1
@@ -1,9 +1,10 @@
-#### Sample for moving the data of a sample Vault into a new target Vault into a new sub-folder
+#### Sample for moving the data of a sample Vault into a new target Vault into a new sub-folder   ##########
 ##   by loading a BCP-package into the powerLoad database (IDB)
 ##   moving all design files and folders into new sub-folder 'Division_007'
 ##   and cleaning some files and folders.
-## This sample uses the BCP-package "Vault_Samples_F".
-#### No guarantee of correctness and up-to-dateness!
+##
+##  To use this sample you need to create or have a BCP-package that contains a design folder "Designs".
+#### No guarantee of correctness and up-to-dateness!    ####################################################
 
 
 # Load powerLoad modules 
@@ -13,17 +14,19 @@ Get-ChildItem -Path $powerLoadPath -Filter powerload.psd1 | Unblock-File   #Unbl
 Get-ChildItem -Path $powerLoadPath -Filter *.dll -Recurse | Unblock-File   #Unblocks DLLs if windows blocked the downloaded ZIP
 Import-Module ($powerLoadPath + "\powerload.psd1") -Verbose
 
-### General settings
-$LogPath = "C:\coolOrange\powerLoad-csv-logs\"  #Folder for CSV-log files
-$BCPFolder = "Vault_Samples_F"                  #Folder with BCP-package to import
+### General settings (must be adjusted to your environment!)
+$LogPath = "C:\coolOrange\powerLoad-csv-logs\"               #Folder for CSV-log files
+$BCPFolder = "MyBCP-package"                                 #TODO: Rename to the folder name of your BCP-package to import
+$ImportPath = "C:\temp\VaultBCP\ExportPackages\$BCPFolder"   #DTU-Export location
+$ExportPath = "C:\temp\VaultBCP\pL-Exports\$BCPFolder"       #powerLoad-Export location
 ## Connection to SQL Instance
 $SQLServer = "(local)\AUTODESKVAULT"
-$IDB = "pL_BCP_Demo"                            #powerLoad database
+$IDB = "pL_BCP_Sample"                            #powerLoad database
 $SQLUser = "SA"
 $SAPassword = "AutodeskVault@26200"
 ## Connection to Target Vault
 $VaultServer = "localhost"                      #Target server name
-$VaultDB = "Vault_Target"                       #Name of target Vault DB
+$VaultDB = "Vault_Target"                       #Name of your target Vault DB
 $VaultUser = "Administrator"
 $VaultPW = ""
 
@@ -31,7 +34,7 @@ $VaultPW = ""
 Connect-powerLoadDatabase -Server $SQLServer -DatabaseName $IDB -User $SQLUser -Password $SAPassword -ForceDatabaseCreation
 
 # Import BCP-package to powerLoad db
-$result = Import-BCP -ImportPath "C:\temp\VaultBCP\ExportPackages\$BCPFolder"
+$result = Import-BCP -ImportPath $ImportPath
 ### Review import:
 ## All files are in table 'Files'. The path to the file resides in field 'LocalFullFileName'
 ## Review system fields and UDPs
@@ -48,14 +51,7 @@ Invoke-PowerLoadSql -Statement "INSERT INTO [dbo].[Folders] ([FolderID], [VaultF
 Invoke-PowerLoadSql -Statement "UPDATE Folders SET VaultPath = REPLACE(VaultPath, '$/Designs/', '$/Designs/Division_007/') FROM Folders where VaultPath like '$/Designs/%'"
 # Correct ParentFolderID (folder structure is defined by 'VaultPath')
 Invoke-PowerLoadSql -Statement "UPDATE Folders SET ParentFolderID = NULL where ParentFolderID = (SELECT FolderID FROM Folders where VaultFolderName = 'Designs')"
-# DELETE Folder TEST and all its files and their FileFileRelations
-Invoke-PowerLoadSql -Statement "DELETE FROM FileFileRelations where ParentFileID in (SELECT FileID FROM Files where FolderID in (SELECT FolderID FROM Folders where VaultFolderName = 'TEST'))"
-Invoke-PowerLoadSql -Statement "DELETE FROM Files where FolderID in (SELECT FolderID FROM Folders where VaultFolderName = 'TEST')"
-Invoke-PowerLoadSql -Statement "DELETE FROM Folders where VaultFolderName = 'TEST'"
-# Move (DWG) files from folder 'Nozzle' to folder 'AutoCAD'
-Invoke-PowerLoadSql -Statement "UPDATE Files SET FolderID = (SELECT FolderID FROM Folders where VaultFolderName = 'AutoCAD') where FolderID = (SELECT FolderID FROM Folders where VaultFolderName = 'Nozzle')"
-Invoke-PowerLoadSql -Statement "DELETE FROM Folders where VaultFolderName = 'Nozzle'"
-# Set ('PDF', 'BMP', 'JPG') to 'Design Representation' and set behaviours
+# Set ('PDF', 'BMP', 'JPG') to 'Design Representation' and set behaviours, if your package contains 'PDF', 'BMP', 'JPG'
 Invoke-PowerLoadSql -Statement "UPDATE Files SET Category = 'Design Representation' where FileExtension in ('PDF', 'BMP', 'JPG') and Category = 'Base'"
 Invoke-PowerLoadSql -Statement "UPDATE Files SET LifecycleState = 'Released' where Category = 'Design Representation'"
 Invoke-PowerLoadSql -Statement "UPDATE Files SET LifecycleDefinition = 'Design Representation Process' where Category = 'Design Representation'"
@@ -71,11 +67,11 @@ Test-PowerLoadDataBase -ErrorCSVPath ($LogPath + "IDB_errors.csv")
 Test-PowerLoadVaultBehaviors -Server $VaultServer -Vault $VaultDB -User $VaultUser -Password $VaultPW -ErrorCSVPath ($LogPath + 'VLTBehaviors_errors.csv')
 
 # Export IDB to BCP-package
-$result = Export-BCP -ExportDirectory "C:\temp\VaultBCP\pL-Exports\$BCPFolder" -DisableConfig 1 
+$result = Export-BCP -ExportDirectory $ExportPath -DisableConfig 1 
 
-## Import BCP-package to target Vault
+## Import BCP-package to target Vault with DTU
 
 
 ### General hints
 # Showing parameter for cmdlet
-Get-Help Resolve-InventorReferences
+Get-Help Export-BCP
