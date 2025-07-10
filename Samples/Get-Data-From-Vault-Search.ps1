3e9b14a5bc6bcee51c75758af98a9d34bf04f76f
Markus Dössinger <36193155+MarkusCool@users.noreply.github.com>
2025-07-10 16:32:31 +0200

    Update Get-Data-From-Vault-Search.ps1

--- Samples/Get-Data-From-Vault-Search.ps1
+++ Samples/Get-Data-From-Vault-Search.ps1
@@ -1,11 +1,15 @@
-$powerLoadPath = "c:\temp\powerLoad\powerload.dll"
-Get-ChildItem -Path $powerLoadPath -Filter *.dll -Recurse | Unblock-File
-Import-Module $powerLoadPath
+$VaultVersion = "2025"                                                            #Version of target Vault
+$powerLoadPath = "C:\Program Files\coolOrange\powerload\Vault_" + $VaultVersion   #path to the powerLoad psd1
+Import-Module ($powerLoadPath + "\powerload.psd1") 
 
 # define your database connection
-Connect-PowerLoadDatabase -Server 'localhost\powerload' -User 'sa' -Password '' -DatabaseName 'test' -ForceDatabaseCreation
+Connect-PowerLoadDatabase -Server 'localhost\AUTODESKVAULT' -DatabaseName 'pl-test' -User 'sa' -Password 'AutodeskVault@26200' -ForceDatabaseCreation
 
 # collect all files from Vault base on the SearchName criteria
 Get-VaultData -VaultServer 'localhost' -VaultName 'Vault' -Username 'Administrator' -Password '' -SearchName 'SearchName' -DownloadPath 'c:\temp\VaultFiles' -ExactVersionOnly
 
-Export-BCP -ExportDirectory 'c:\temp\VaultSearchBcp'
+$dbCheck = Test-PowerLoadDataBase 
+
+$vaultCheck = Test-PowerLoadVaultBehaviors -Server "localhost" -Vault "Vault" -User "Administrator" -Password "" 
+
+Export-BCP -ExportDirectory 'c:\temp\test_export'          #path must exist
